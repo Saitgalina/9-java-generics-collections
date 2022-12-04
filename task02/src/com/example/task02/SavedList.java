2cf19f5547c36039ee15b439dbd1db8163e51ed4
Vilena <vilena.saitgalina@mail.ru>
2022-12-04 12:33:33 +0500

    Саитгалина Вилена task2

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,62 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private List<E> elements;
+    private File file;
 
     public SavedList(File file) {
+        this.file = file;
+        if (!file.exists())
+            elements = new ArrayList<>();
+        else {
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+                elements = (List<E>) objectInputStream.readObject();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void write(){
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))){
+            objectOutputStream.writeObject(elements);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return elements.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = elements.set(index,element);
+        write();
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return elements.size();
     }
 
     @Override
     public void add(int index, E element) {
+        elements.add(index,element);
+        write();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E elem = elements.remove(index);
+        write();
+        return elem;
     }
 }
